63f9758223c6c02b853e89b80632cb28c359d4c3
DelfiLinares <Alumno26.Linares.Pataro.Delfina@ipm.edu.ar>
2026-04-07 15:14:35 -0300

    movimiento y espejado de frame

--- beat-em-up-unbeatable-edition/scenes/scripts/beat.gd
+++ beat-em-up-unbeatable-edition/scenes/scripts/beat.gd
@@ -1,30 +1,25 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 
-var direccion = Vector2.ZERO
+var direction = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	var direction_x = Input.get_action_strength("rightArrowKey") - Input.get_action_strength("leftArrowKey")
-	velocity.x = direction_x * SPEED
+	direction.x = Input.get_action_strength("rightArrowKey") - Input.get_action_strength("leftArrowKey")
+	direction.y = Input.get_action_strength("downArrowKey") - Input.get_action_strength("upArrowKey")
+	velocity = direction * SPEED
+		 
+	if direction.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	elif direction.x < 0:
+		$AnimatedSprite2D.flip_h = true
 	
-	
-	if direction_x != 0:
-		print(direction_x)
-	
-	if direction_x:
+	if direction.length() > 0:
 		$AnimatedSprite2D.play("runSide")
 		move_and_slide()
-			
+	else: 
+		$AnimatedSprite2D.play("idle")
+		
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	
-	
-	
-
-	
