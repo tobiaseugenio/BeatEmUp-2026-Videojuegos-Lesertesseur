b9ad786fe5400ea44493e8717700b2bfaa8bfd54
Eugeles <eufrales@gmail.com>
2026-04-09 21:49:27 -0300

    reestructuracion del codigo de sprites, falta detener la animacion del golpe

--- beat-em-up-unbeatable-edition/scenes/scripts/beat.gd
+++ beat-em-up-unbeatable-edition/scenes/scripts/beat.gd
@@ -5,25 +5,57 @@ extends CharacterBody2D
 
 var direction = Vector2.ZERO
 
+enum State {COMBAT, DAMAGE, FALL, IDLE, JUMP, RUN, WAKEUP}
+var state = State.IDLE
+var loopAnimations = [State.IDLE, State.RUN]
+const stateAnimations={
+	State.IDLE : "idle",
+	State.RUN:    "runSide",
+	State.COMBAT: "combat",
+	State.JUMP:   "jump",
+	State.DAMAGE: "damage",
+	State.WAKEUP: "wakeup",
+}
+
 func _physics_process(delta: float) -> void:
+	input()
+	movement()
+	animation()
+	flipSprite()
+	move_and_slide()
+
+func movement():
+	if state == State.COMBAT:
+		return
+	
+	if velocity.length() == 0:
+		state = State.IDLE
+	else:
+		state = State.RUN
+		
+func input():
 	direction.x = Input.get_action_strength("dKey") - Input.get_action_strength("aKey")
 	direction.y = Input.get_action_strength("sKey") - Input.get_action_strength("wKey")
+
 	velocity = direction * SPEED
-		 
+	
+	if Input.is_action_just_pressed("spacebar"):
+		state = State.COMBAT
+		
+func animation():
+	var animation = stateAnimations.get(state, "idle")
+	if $AnimatedSprite2D.animation != animation:
+		$AnimatedSprite2D.play(animation)
+		var cantSprites = $AnimatedSprite2D.get_sprite_frames()
+		print(cantSprites)
+		
+func flipSprite():
 	if direction.x > 0:
 		$AnimatedSprite2D.flip_h = false
 	elif direction.x < 0:
 		$AnimatedSprite2D.flip_h = true
-	
-	if Input.is_action_just_pressed("spacebar"):
-		combat()
-	elif direction.length() > 0:
-		$AnimatedSprite2D.play("runSide")
-		move_and_slide()
-	else: 
-		$AnimatedSprite2D.play("idle")
 		
-func combat():
-	if Input.is_action_pressed(	"spacebar"):
-		$AnimatedSprite2D.play("attack")
-		print("barra espaciadora")
+#func _on_animated_sprite_2d_animation_finished() -> void:
+	#if state == State.COMBAT:
+		#state = State.IDLE
+		#print("_on_animated_sprite_2d_animation_finished state -> ",  state)
